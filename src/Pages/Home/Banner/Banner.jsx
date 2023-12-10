ef20e6ff7bf65b7ac73c1a57d25f8e8d50ee13f3
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-12-10 13:35:26 -0800

    Added a download functionality to download the resume

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import arifulIslam from '../../../assets/Ariful Islam (1) (1).png'
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
 
 const Banner = () => {
+
+    const downloadResume = () => {
+        // Google drive resume link
+        const fileUrl = 'https://drive.google.com/uc?export=download&id=1HmzCtzNx4hbY9iPrARA8UeATtQPLrr70';
+      
+        // Create an anchor element
+        const link = document.createElement('a');
+      
+        // Set the href attribute to the file URL
+        link.href = fileUrl;
+      
+        // Set the download attribute with the desired filename
+        link.download = 'ResumeOfficial.pdf';
+      
+        // Append the anchor element to the document body
+        document.body.appendChild(link);
+      
+        // Trigger a click on the anchor element to start the download
+        link.click();
+      
+        // Remove the anchor element from the document
+        document.body.removeChild(link);
+      };
+
+      
     return (
         <div className='h-[calc(100vh-64px)] flex items-center justify-center py-8'>
             <div className="flex-1 space-y-4">
                 <p className='text-[#03045E] font-medium'>Hello, I'm Ariful Islam,</p>
                 <h1 className='text-8xl font-bold leading-[100px] text-[#03045E]'>Web Developer</h1>
                 <p className='text-[#03045E] font-medium'>Transforming Ideas into Digital Realities with Innovation and Precision.</p>
+                <AwesomeButton cssModule={'aws'} type="secondary"><a onClick={downloadResume}>Resume</a></AwesomeButton>
             </div>
             <div className="flex-1 h-full">
                 <img src={arifulIslam} alt="" className=' object-cover h-full w-full bg-bottom'/>
